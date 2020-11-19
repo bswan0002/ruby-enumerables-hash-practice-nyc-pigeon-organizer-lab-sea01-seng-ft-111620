484478d63f2b5cd1e7dbc030ffcb74e9e71999b5
Ben Swanson <bswan0002@gmail.com>
2020-11-19 02:37:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,5 @@
+
+
 def nyc_pigeon_organizer(pigeon_data)
   myHash = {}
   
@@ -11,4 +13,23 @@ def nyc_pigeon_organizer(pigeon_data)
     end
   end
   myHash
+end
+
+
+def nyc_pigeon_organizer(data)
+  
+ organized_list={}
+ 
+ data.each do |outer_key,values|  #Access lvl1
+  values.each do |inner_key,p_names|  #Access lvl2
+   p_names.each do |name| #Access lvl3
+    if organized_list[name] ||={}  #using||= to check for nil assign empty hash
+     if organized_list[name][outer_key] ||= []  #using||= to check for nil assign empty array
+      organized_list[name][outer_key]<< (inner_key.to_s)  #Convert symbol to strings
+    end
+    end
+   end
+   end
+ end
+ organized_list
 end
